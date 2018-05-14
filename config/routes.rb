da423019e16775e8b0fe2ff0363a44d61109d00d
sekishin <sekishin1214@gmail.com>
2018-05-14 17:04:52 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
@@ -7,5 +8,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#goodbye'
-  root 'users#index'
+  # root 'users#index'
 end
