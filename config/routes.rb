08f1cea205f531197c3b158176b5a86bfaea28f7
sekishin <sekishin1214@gmail.com>
2018-05-28 15:25:11 +0900

    destroy scaffold micropost

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  'signup',   to: 'users#new'
-  resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#goodbye'
   # root 'users#index'
